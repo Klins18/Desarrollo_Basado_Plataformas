d474ce4e5db39d933c65041a73864831621dafca
Klins18 <102160876+Klins18@users.noreply.github.com>
2023-11-14 07:37:51 -0500

    Actualizar app.py

--- Todo_list/todo list/app.py
+++ Todo_list/todo list/app.py
@@ -1,12 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("todolist.html")
-@app.route('/redireccionar')
-def redireccionar():
-    return redirect(url_for(' .html'))
-if __name__== '__main__':
-    app.run(debug=True)
+    return render_template("index.html")
+
